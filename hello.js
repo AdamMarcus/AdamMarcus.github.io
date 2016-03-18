937573e4843b84879eb51b3a886c5dcb53229166
Adam Austin <adam.marcus.austin@gmail.com>
2016-03-17 18:26:42 -0600

    Made small changes to hello.js and index.html when trying to learn functions and stuff

--- hello.js
+++ hello.js
@@ -1,2 +1,8 @@
-// File to hold Java Script
-alert("hello World");
+// // File to hold Java Script
+
+window.onload = function(){sweetAlert("hi");}
+
+var add = function(var1, var2)
+{
+  return var1 + var2;
+}
