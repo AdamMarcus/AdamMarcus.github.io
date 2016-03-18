660bbb0b25ecd7ab0c674602fd1d198dd28021fb
Adam Austin <adam.marcus.austin@gmail.com>
2016-03-17 21:30:44 -0600

    Added some input slots at the top of the page and messed with a function called add

--- hello.js
+++ hello.js
@@ -1,8 +1,21 @@
 // // File to hold Java Script
 
-window.onload = function(){sweetAlert("hi");}
+// window.onload = function(){sweetAlert("hi");}
 
-var add = function(var1, var2)
-{
-  return var1 + var2;
+var add = function() {
+  var numOne = document.getElementById('num1').value;
+  var numTwo = document.getElementById('num2').value;
+  alert(numOne + numTwo);
+  // now here set the value of an html element to be "Number(numOne) + Number(numTwo)"
 }
+
+
+var putInAlert = function (input)
+{
+  sweetAlert(input);
+}
+
+// var add = function(var1, var2)
+// {
+//   return var1 + var2;
+// }
